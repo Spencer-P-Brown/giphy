a7a005e16014e959443dd91ec2401c8b2e9ad93b
Spencer-P-Brown <Spencer-P-Brown@github.com>
2017-05-17 19:16:20 -0500

    started building js

---
+++ assets/javascript/script.js
@@ -0,0 +1,72 @@
+// this is the giphy key dc6zaTOxFJmzC
+
+/*
+api.giphy.com
+/v1/gifs/search
+
+*/
+$(document).ready(function() {
+    var topics = ["Nic Cage", "Crispin Glover", "Gary Busey", "Klaus Kinski"];
+
+    buttonShow();
+
+	function displayGif() {
+
+       var animal = $(this).attr("data-name");
+       var queryURL = "http://api.giphy.com/v1/gifs/random?" +
+           animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+       $.ajax({
+           url: queryURL,
+           method: "GET"
+       })
+
+       .done(function(response) {
+           var results = response.data;
+
+           for (var i=0; i < results.length; i++) {
+
+               if (results[i].rating) {
+                   var gifDiv = $("<div class='item'>");
+                   var rating = results[i].rating;
+                   var p = $("<p>").text("Rating: " + rating);
+                   var animalImage = $("<img>");
+                       animalImage.attr("src", results[i].images.fixed_height_still.url);
+
+                   gifDiv.append(p);
+                   gifDiv.append(animalImage);
+
+                   $("#display-gifs").prepend(gifDiv);
+               }
+           }
+       })
+   }
+
+    //This is the code that makes button adding dynamic
+    function buttonShow(){
+
+    	$("#display-btns").empty();
+    	for (var i = 0; i < topics.length; i ++) {
+    		var a = $("<button>");
+    		a.addClass("gif");
+    		a.attr("data-name", topics[i]);
+    		a.text(topics[i]);
+    		$("#display-btns").append(a);
+    	}
+    }
+	
+	$("#add-gif").on("click", function(event){
+		event.preventDefault();
+		var gif = $("#gif-input").val().trim();
+		topics.push(gif);
+		buttonShow();
+		$("#add-a-btn")[0].reset();	
+	});
+
+	$(document).on("click", ".gif", displayGif);
+
+
+
+
+
+});
